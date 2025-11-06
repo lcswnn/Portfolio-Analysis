57d899fa613357099e0e8e9cfe8a35bb1a8f1ddc
Lucas Waunn <lucaswaunn@gmail.com>
2025-11-06 00:13:11 -0600

    added two more data sets to show passing of time into the future by weeks.

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-data = pd.read_csv('Positions_Dummy.csv', skiprows=3)
+data = pd.read_csv('/Users/lucaswaunn/projects/Portfolio-Analysis/positions/Positions_Dummy.csv', skiprows=3)
+data_formatted = data.iloc[:, :-1]
+data2 = pd.read_csv('/Users/lucaswaunn/projects/Portfolio-Analysis/positions/Positions_Dummy.csv', skiprows=3)
+data2_formatted = data2.iloc[:, :-1]
+data3 = pd.read_csv('/Users/lucaswaunn/projects/Portfolio-Analysis/positions/Positions_Dummy.csv', skiprows=3)
+data3_formatted = data3.iloc[:, :-1]
 data = data.dropna(how='all')  # Drop rows where all elements are NaN
-print(data.head())
+print(data_formatted, data2_formatted, data3_formatted)
 
 
 
